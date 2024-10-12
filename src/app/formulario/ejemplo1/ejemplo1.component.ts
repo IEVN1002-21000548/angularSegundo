4cc34740007060d849e7d5837047e75818fe969c
Ricardo Alexis Hernandez Rodriguez <21000548@alumnos.utleon.edu.mx>
2024-10-12 10:25:58 -0600

    Rutas

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'; 
 
 
 interface Usuarios{
@@ -11,12 +11,20 @@ interface Usuarios{
 @Component({
   selector: 'app-ejemplo1',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, Ejemplo1Component],
   templateUrl: './ejemplo1.component.html',
   styleUrl: './ejemplo1.component.css'
 })
 export class Ejemplo1Component {
-  formGroup:FormGroup;
+  formGroup!:FormGroup;
+
+  materia='pwa'
+  tem=''
+  alumnos:Usuarios={
+    nombre: '',
+    edad: 0,
+    email: ''
+  }
 
   constructor(private fb:FormBuilder){}
   ngOnInit(): void {
@@ -30,6 +38,25 @@ export class Ejemplo1Component {
     })
   }
   onSubmit():void{
+    const{nombre, edad, email}=this.formGroup.value;
+    this.alumnos.nombre=nombre;
+    this.alumnos.edad=edad;
+    this.alumnos.email=email;
+    let alumnoJSON=JSON.stringify(this.alumnos);
+
     console.log(this.formGroup.value);
+
+    localStorage.setItem('materia', this.materia);
+    localStorage.setItem('alumno', alumnoJSON);
+  }
+
+  subImprimir():void{
+    this.tem=localStorage.getItem('materia')!
+    console.log(this.alumnos);
+
+    const alumnoGuardado=localStorage.getItem('alumno')
+    if(alumnoGuardado){
+      const alumno:Usuarios=JSON.parse(alumnoGuardado)
+    }
   }
 }
